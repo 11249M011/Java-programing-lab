33abb89a396b7d519cd989a62c681a313931100e
11249M011 <11249M011@kanchiuniv.ac.in>
2026-08-07 11:05:48 +0530

    Class & object

--- Student.java
+++ Student.java
@@ -1,22 +1,23 @@
-class Student{
+class Student
+{
     String name;
     int rollNo,marks;
-
-    Student(String n,int r,int m){
+    Student(String n,int r,int m)
+    {
         name=n;
         rollNo=r;
         marks=m;
     }
-
-    void display(){
+    void display()
+    {
         System.out.println("Name: "+name);
         System.out.println("Roll No: "+rollNo);
         System.out.println("Marks: "+marks);
         System.out.println("Grade: "+grade());
         System.out.println();
     }
-
-    String grade(){
+    String grade()
+    {
         if(marks>=90)
             return "A";
         else if(marks>=75)
@@ -29,12 +30,12 @@ class Student{
             return "F";
     }
 }
-
-public class m002student{
-    public static void main(String[] args){
+public class Student
+{
+    public static void main(String[] args)
+    {
         Student s1=new Student("Rahul",101,92);
         Student s2=new Student("Priya",102,68);
-
         s1.display();
         s2.display();
     }
